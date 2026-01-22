59ee53c990b38f79ab91d9ae536645d8ff1a3c11
takuya-isbs <57710912+takuya-isbs@users.noreply.github.com>
2026-01-23 01:26:34 +0900

    add macos-15
    Closes #6.
    
    Signed-off-by: takuya-isbs <57710912+takuya-isbs@users.noreply.github.com>

--- Formula/jwt-agent.rb
+++ Formula/jwt-agent.rb
@@ -1,8 +1,8 @@
 class JwtAgent < Formula
   desc "Obtain a JSON Web Token (JWT) from a JWT server"
   homepage "https://github.com/oss-tsukuba/jwt-agent"
-  url "https://github.com/oss-tsukuba/jwt-agent/archive/refs/tags/1.1.1.tar.gz"
 
+  url "https://github.com/oss-tsukuba/jwt-agent/archive/refs/tags/1.1.1.tar.gz"
   # shasum -a 256
   sha256 "7b3bb160e963feed83c63018c54056643cf6eb7510a431c15c931756ba5ce4cb"
 
