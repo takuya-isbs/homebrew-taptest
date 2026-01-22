2093866fdea78e14323f4bf32f22ecb263631ef1
takuya-isbs <57710912+takuya-isbs@users.noreply.github.com>
2026-01-22 21:23:15 +0900

    fix "brew style" errors

--- Formula/jwt-agent.rb
+++ Formula/jwt-agent.rb
@@ -1,11 +1,8 @@
 class JwtAgent < Formula
-  desc "obtain a JSON Web Token (JWT) from a JWT server"
+  desc "Obtain a JSON Web Token (JWT) from a JWT server"
   homepage "https://github.com/oss-tsukuba/jwt-agent"
 
-  v_num = "1.1.1"
-  version v_num
-
-  url "https://github.com/oss-tsukuba/jwt-agent/archive/refs/tags/#{v_num}.tar.gz"
+  url "https://github.com/oss-tsukuba/jwt-agent/archive/refs/tags/1.1.1.tar.gz"
 
   # shasum -a 256
   sha256 "7b3bb160e963feed83c63018c54056643cf6eb7510a431c15c931756ba5ce4cb"
